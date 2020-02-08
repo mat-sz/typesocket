97dadf8339f115b3dae2f28b80812688c60beb39
Mat Sz <contact@matsz.dev>
2020-02-08 17:13:04 +0100

    added more information about the options

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,66 @@
+/**
+ * TypeSocket options.
+ * @interface
+ */
 export interface TypeSocketOptions {
+    /**
+     * Maximum amount of retries until a successful connection gets established.
+     * Set to 0 to keep retrying forever.
+     * Default: 5
+     */
     maxRetries?: number,
+    
+    /**
+     * When set to true the client will try to reconnect when the socket is intentionally closed by either party.
+     * Default: false
+     */
     retryOnClose?: boolean,
+
+    /**
+     * Time between retries. (ms)
+     * Default: 500
+     */
     retryTime?: number,
 };
 
+/**
+ * TypeSocket class.
+ */
 export class TypeSocket<T> {
+    /**
+     * Function that is called when a connection is successfully established.
+     */
     onConnected?: () => void;
+
+    /**
+     * Function that is called when the socket is disconnected.
+     */
     onDisconnected?: () => void;
+
+    /**
+     * Function that is called when the socket is permanently disconnected (after running out of retries or being manually disconnected).
+     */
     onPermanentlyDisconnected?: () => void;
+
+    /**
+     * Function that is called when the message is received.
+     * @param {T} message 
+     */
     onMessage?: (message: T) => void;
+
+    /**
+     * The WebSocket
+     */
     private socket: WebSocket | null = null;
+
+    /**
+     * Temporary retry counter.
+     */
     private retries = 0;
+
+    /**
+     * Instance options.
+     */
     private options: TypeSocketOptions = {
         maxRetries: 5,
         retryOnClose: false,
